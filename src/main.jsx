38099e0eb018e528d7ae369d8a2581519a9c0660
etimelli2-ai <etimelli2@gmail.com>
2026-07-31 18:09:35 +0200

    Add ThemeProvider to main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -5,6 +5,7 @@ import { QueryClient, QueryClientProvider } from '@tanstack/react-query'
 import { Toaster } from 'react-hot-toast'
 import App from './App'
 import { AuthProvider } from './hooks/useAuth'
+import { ThemeProvider } from './hooks/useTheme'
 import './index.css'
 
 const queryClient = new QueryClient({
@@ -18,11 +19,13 @@ const queryClient = new QueryClient({
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <BrowserRouter>
-    <QueryClientProvider client={queryClient}>
-      <AuthProvider>
-        <App />
-        <Toaster position="top-center" />
-      </AuthProvider>
-    </QueryClientProvider>
+    <ThemeProvider>
+      <QueryClientProvider client={queryClient}>
+        <AuthProvider>
+          <App />
+          <Toaster position="top-center" />
+        </AuthProvider>
+      </QueryClientProvider>
+    </ThemeProvider>
   </BrowserRouter>
 )
