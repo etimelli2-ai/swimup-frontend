94d4727d227c92b69f3d21358c17968f7aee27eb
etimelli2-ai <etimelli2@gmail.com>
2026-08-05 15:56:38 +0200

    Add Boutique and AdminBoutique routes

--- src/App.jsx
+++ src/App.jsx
@@ -8,11 +8,13 @@ import MonAvis from './pages/MonAvis'
 import Profil from './pages/Profil'
 import Portefeuille from './pages/Portefeuille'
 import Loterie from './pages/Loterie'
+import Boutique from './pages/Boutique'
 import AdminDashboard from './pages/admin/AdminDashboard'
 import AdminAvis from './pages/admin/AdminAvis'
 import AdminUsers from './pages/admin/AdminUsers'
 import AdminRetraits from './pages/admin/AdminRetraits'
 import AdminLoterie from './pages/admin/AdminLoterie'
+import AdminBoutique from './pages/admin/AdminBoutique'
 import ClientDashboard from './pages/client/ClientDashboard'
 import ClientPaiement from './pages/client/ClientPaiement'
 import ClientCommandes from './pages/client/ClientCommandes'
@@ -43,16 +45,19 @@ export default function App() {
       <Route path="/login"    element={<Login />} />
       <Route path="/register" element={<Register />} />
 
+      {/* Routes membres */}
       <Route path="/" element={<PrivateRoute><Layout /></PrivateRoute>}>
-        <Route index                element={<Dashboard />} />
-        <Route path="dashboard"     element={<Dashboard />} />
-        <Route path="avis"          element={<Avis />} />
-        <Route path="mon-avis"      element={<MonAvis />} />
-        <Route path="portefeuille"  element={<Portefeuille />} />
-        <Route path="profil"        element={<Profil />} />
-        <Route path="loterie"       element={<Loterie />} />
+        <Route index               element={<Dashboard />} />
+        <Route path="dashboard"    element={<Dashboard />} />
+        <Route path="avis"         element={<Avis />} />
+        <Route path="mon-avis"     element={<MonAvis />} />
+        <Route path="portefeuille" element={<Portefeuille />} />
+        <Route path="profil"       element={<Profil />} />
+        <Route path="loterie"      element={<Loterie />} />
+        <Route path="boutique"     element={<Boutique />} />
       </Route>
 
+      {/* Routes admin */}
       <Route path="/admin" element={<PrivateRoute roles={['admin']}><Layout /></PrivateRoute>}>
         <Route index           element={<AdminDashboard />} />
         <Route path="avis"     element={<AdminAvis />} />
@@ -60,8 +65,10 @@ export default function App() {
         <Route path="retraits" element={<AdminRetraits />} />
         <Route path="loterie"  element={<AdminLoterie />} />
         <Route path="commande" element={<ClientDashboard />} />
+        <Route path="boutique" element={<AdminBoutique />} />
       </Route>
 
+      {/* Routes client */}
       <Route path="/client" element={<PrivateRoute roles={['client','admin']}><Layout /></PrivateRoute>}>
         <Route index            element={<ClientDashboard />} />
         <Route path="payer"     element={<ClientPaiement />} />
